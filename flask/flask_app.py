d7ee285fa132be6cd4293a9eb6feaf2f326b2bef
Aijaz Ansari <aijaz@aijaz.net>
2024-05-30 11:57:31 -0600

    Added postgres

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import psycopg2
+import psycopg2.extras
 
 my_flask_app = Flask(__name__)
 
@@ -6,6 +8,29 @@ my_flask_app = Flask(__name__)
 @my_flask_app.route("/cloe")
 def cloe():
     return "Hello, Cloe"
+
+
+@my_flask_app.route("/employees")
+def employees():
+    # connect to the database
+    con = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="mysecretpassword",
+        host="aijaz_pg_c"
+    )
+
+    # create a cursor
+    cur = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    # get the data
+    cur.execute("select * from employee")
+    result = cur.fetchall()
+
+    # commit and close the database connection
+    con.close()
+    # return the data
+    return result
 
 
 if __name__ == "__main__":
